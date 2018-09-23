746fe4f1a2670c72713c1587fda41b73d4f5d7e8
joewledger <joewledger@gmail.com>
2018-09-23 13:57:09 -0400

    Add test for number of tiles in a sector
    Signed-off-by: joewledger <joewledger@gmail.com>

--- tests/test_map.py
+++ tests/test_map.py
@@ -1,3 +1,7 @@
+from gaia.map import Sector
+
 
 def test_sector_has_correct_number_of_tiles():
-    assert False
+    assert len(Sector([], radius=1).hexagons) == 1
+    assert len(Sector([], radius=2).hexagons) == 7
+    assert len(Sector([], radius=3).hexagons) == 19
