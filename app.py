c4155b7b40eb66656b7dec868a166c1b03d4b78c
Joe Ledger <joewledger@gmail.com>
2018-11-27 03:59:22 -0500

    WIP render map
    Signed-off-by: Joe Ledger <joewledger@gmail.com>

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from gaia.map import Map
 
 import os
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 app = Flask(__name__)
 
 config_path = os.path.dirname(os.path.realpath(__file__)) + "\\configs\\board.json"
 
 
+@app.route('/')
+def main():
+    map = map = Map.load_from_config(config_path, game_type="2p_default")
+    return render_template("test.html", map=map)
+
+
 @app.route('/map')
 def get_map():
-    map_json = Map.load_from_config(config_path, game_type="2p_default").to_json()
+    map = Map.load_from_config(config_path, game_type="2p_default")
 
-    return Response(response=map_json,
+    return Response(response=map.to_json(),
                     status=200,
                     mimetype="application/json")
