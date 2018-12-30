bb6c5d8dcb7cc6fad6e71bc7e43dc2f871d5c6d4
Joe Ledger <joewledger@gmail.com>
2018-12-30 18:19:38 -0500

    Render sectors without planets using react
    Signed-off-by: Joe Ledger <joewledger@gmail.com>

--- static/js/map.js
+++ static/js/map.js
@@ -24,21 +24,65 @@ class Planet extends React.Component {
   };
 };
 
+class Sector extends React.Component {
+  render() {
+    let size = this.props.size;
+
+    let sector_x_factor = this.props.screen_x_factor * size * .01;
+    let sector_y_factor = this.props.screen_y_factor * size * .01;
+    let sector_translate = `translate(${sector_x_factor},${sector_y_factor})`;
+
+    let hexagons = [];
+
+    this.props.hexagons.forEach(function(hexagon) {
+      let hexagon_x = hexagon.screen_x_factor * size;
+      let hexagon_y = hexagon.screen_y_factor * size;
+
+      hexagons.push(<Hexagon x={hexagon_x} y={hexagon_y} />);
+    });
+
+    return (
+      <g transform={sector_translate}>{hexagons}</g >
+    );
+  };
+};
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      size: 100,
+      sectors: [],
+      federations: []
+    };
+  };
+
   componentDidMount() {
     fetch('map').then(results => {
       return results.json();
     }).then(data => {
-      console.log(data);
+      this.setState({
+        sectors: data.sectors,
+        federations: data.federations
+      });
     });
-  }
+  };
 
   render() {
+    let sectors = [];
+    this.state.sectors.forEach(sector => {
+      sectors.push(<Sector hexagons={sector.hexagons}
+                           planets={sector.planets}
+                           size={100}
+                           screen_x_factor={sector.screen_x_factor}
+                           screen_y_factor={sector.screen_y_factor} />);
+      }
+    );
+
     return (
       <svg viewBox="-3000 -1500 6000 3000" xmlns="http://www.w3.org/2000/svg">
-        <Hexagon x={0} y={1000} />
-        <Planet x={0} y={1000} hex_color={"#ff00ff"} />
+        {sectors}
       </svg>
     );
   };
