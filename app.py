b108bbe2b74fed84a8dc8ebe613d7e65e4de63f6
remzelg <remycarr@gmail.com>
2018-12-30 23:33:05 -0500

    Adding restplus and barebones move endpoint

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from gaia.map import Map
 
 import os
 from flask import Flask, Response, render_template, request
+from flask_restplus import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
 
 config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "configs", "board.json")
 is_development = os.environ.get("FLASK_DEBUG") == str(1)
 
 
-@app.route('/')
+# View Routes
+@app.route('/board')
 def main():
     return render_template("board.html", development=is_development)
 
@@ -22,3 +26,43 @@ def get_map():
     return Response(response=map.to_json(),
                     status=200,
                     mimetype="application/json")
+
+
+# API
+valid_moves_example_response = [
+    { # Behaves uniquely. Must choose to do before other moves. This must be remembered between player turns. There can be multiple.
+        'type': 'leech',
+        'player_id': '1234',
+        'amount': 3
+    },
+
+    {
+        'type': 'build',
+        'hex': 11,
+        'navigation': {'QIC': 2, 'BON11': 1},
+        'dig': {'DIG': 3, 'ACT4': 2, 'ACT5': 1}
+    },
+
+    {
+        'type': 'upgrade',
+        'hex': 23,
+        'opt1': 1,
+        'opt2': None
+    },
+
+    {
+        'type': 'action',
+        'bonus-action': 4
+    },
+
+    {
+        'type': 'pass',
+        'tiles': [1, 5, 7, 11]
+    }
+]
+
+
+@api.route('/valid moves')
+class ValidMoves(Resource):
+    def get(self):
+        return valid_moves_example_response
