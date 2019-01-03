78e08f4d38bb76753a0eb471b7f468c220a1cc02
Joe Ledger <joewledger@gmail.com>
2019-01-03 01:29:54 -0500

    Refactor turn_validator tests into parametrize
    Signed-off-by: Joe Ledger <joewledger@gmail.com>

--- tests/test_turn_validator.py
+++ tests/test_turn_validator.py
@@ -1,45 +1,47 @@
+import pytest
+
 from gaia.turn_validator import Turn
 from gaia.actions import PlaceMineAction, PassAction, GaiaformAction
 from gaia.map import Hexagon
 
 
-def test_early_terminators_are_illegal(starting_gamestate):
-    actions = [
-        PlaceMineAction(Hexagon(0, 0)),
-        PassAction()
-    ]
-
+@pytest.mark.parametrize("actions,should_be_valid,main_reason", [
+    (
+        [
+            PlaceMineAction(Hexagon(0, 0)),
+            PassAction()
+        ],
+        False,
+        "PlaceMineAction ends the turn, but was not the last action in the turn."
+    ),
+    (
+        [
+            GaiaformAction(Hexagon(0, 0))
+        ],
+        False,
+        "The last action in a turn must end the turn, but GaiaformAction did not"
+    ),
+    (
+        [],
+        False,
+        "The turn had no actions in it"
+    ),
+    (
+        [
+            GaiaformAction(Hexagon(0, 0)),
+            PassAction
+        ],
+        False,
+        'GaiaformAction is a partial action, and the action that follows it is not valid for that partial action'
+    )
+])
+def test_turn_validity(actions, should_be_valid, main_reason,
+                       starting_gamestate, mocker):
     turn = Turn(actions, starting_gamestate, "p1")
-    valid, reasons = turn.validate()
-    assert not valid
-    assert ("PlaceMineAction ends the turn, but was not the last action in the turn." in reasons)
 
+    mocker.patch.object(turn, '_check_all_actions_are_valid')
+    turn._check_all_actions_are_valid.return_value = (True, [])
 
-def test_last_action_must_ends_turn(patch_check_all_actions, starting_gamestate):
-    actions = [
-        GaiaformAction(Hexagon(0, 0))
-    ]
-
-    turn = patch_check_all_actions(Turn(actions, starting_gamestate, "p1"))
-
-    valid, reasons = turn.validate()
-    assert not valid
-    assert ("The last action in a turn must end the turn, but GaiaformAction did not" in reasons)
-
-
-def test_empty_turn_is_invalid(starting_gamestate):
-    turn = Turn([], starting_gamestate, "p1")
-
-    valid, reasons = turn.validate()
-    assert not valid
-    assert ("The turn had no actions in it" in reasons)
-
-
-def test_partial_actions_has_valid_following_action(patch_check_all_actions, starting_gamestate):
-    actions = [GaiaformAction(Hexagon(0, 0)), PassAction]
-
-    turn = patch_check_all_actions(Turn(actions, starting_gamestate, "p1"))
-
-    valid, reasons = turn.validate()
-    assert not valid
-    assert 'GaiaformAction is a partial action, and the action that follows it is not valid for that partial action' in reasons
+    validity, reasons = turn.validate()
+    assert validity == should_be_valid
+    assert (main_reason in reasons)
